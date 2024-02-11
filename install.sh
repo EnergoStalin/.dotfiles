0e1b48fe65d326b9f6dc76ee83cf30df1efc6bef
EnergoStalin <ens.stalin@gmail.com>
2024-02-11 20:23:08 +0000

    kinda pre setup & profiles

--- install.sh
+++ install.sh
@@ -4,4 +4,16 @@ if timeout 1s xset q &>/dev/null; then
     cd xorg && stow -t "$HOME" */ && cd -
 fi
 
-cd cross && stow -t "$HOME" */ && cd -
+if [[ -z "$(which stow)" ]]; then
+    sudo apt update || true
+    sudo apt install stow -y || true
+
+    sudo pacman -Syy || true
+    sudo pacman -S stow || true
+fi
+
+if ! [[ -z "$MINIMAL" ]]; then
+    cd cross && stow -t "$HOME" */ && cd -
+else [[ -z "$SERVER" ]]
+    cd cross && stow -t "$HOME" tmux/ git/ vim/ && cd -
+fi
