4b2170c066ad41163e971ed19ab6b91fe8ea7eaf
EnergoStalin <ens.stalin@gmail.com>
2024-02-20 17:12:25 +0300

    install: optimize pacman query

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ TO_INSTALL=""
 PACKAGES="$(find . -maxdepth 1 -type d -not -name '.git' -and -not -name '.' -exec basename {} \;)"
 
 for package in $PACKAGES; do
-  if ! [[ -z "$(pacman -Qe | grep -i $package)" ]]; then
+  if ! [[ -z "$(pacman -Qe $package)" ]]; then
     TO_INSTALL="$package $TO_INSTALL"
   fi
 done
