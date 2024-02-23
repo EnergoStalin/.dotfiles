838db8fe227c36ae4e3e8aee4738ca54e30f0236
EnergoStalin <ens.stalin@gmail.com>
2024-02-23 19:56:52 +0300

    nvim(markdown): preview shortcut

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -7,5 +7,7 @@ return {
     vim.g.mkdp_auto_close = 0
     vim.g.mkdp_auto_start = 1
     vim.g.mkdp_images_path = vim.fn.getcwd()
+
+    vim.keymap.set('n', '<leader>mt', '<cmd>MarkdownPreviewToggle<cr>')
   end,
 }
