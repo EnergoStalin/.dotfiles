385ff996405e9187c8d05beda3f5eac72bc75525
EnergoStalin <ens.stalin@gmail.com>
2024-03-02 12:11:01 +0300

    nvim(telescope): ignore list

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,17 @@ return {
   keys = {
     { '<leader>ff', '<cmd>Telescope find_files hidden=True<cr>', desc = '', },
     { '<leader>fg', '<cmd>Telescope live_grep hidden=True<cr>',  desc = '', },
-    { '<leader>fb', '<cmd>Telescope buffers hidden=True<cr>',  desc = '', },
+    { '<leader>fb', '<cmd>Telescope buffers hidden=True<cr>',    desc = '', },
   },
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = {
+          '.git',
+          'node_modules',
+          '.xmake',
+        },
+      },
+    })
+  end,
 }
