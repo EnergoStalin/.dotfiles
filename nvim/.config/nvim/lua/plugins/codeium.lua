4dcfbbddb8581ce852286aa40ab46684d805f5d0
EnergoStalin <ens.stalin@gmail.com>
2024-03-12 17:43:04 +0300

    nvim(codeium): manual 0

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -3,7 +3,7 @@ return {
   event = 'BufEnter',
   config = function()
     vim.g.codeium_disable_bindings = 1
-    vim.g.codeium_manual = 1
+    vim.g.codeium_manual = 0
 
     vim.keymap.set('i', '<tab>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true, })
     vim.keymap.set('i', '<m-]>', function() return vim.fn['codeium#CycleCompletions'](1) end,
