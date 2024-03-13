a5c82d4ee13237cc9d4136f166f2966999c348ce
EnergoStalin <ens.stalin@gmail.com>
2024-03-13 17:23:11 +0300

    nvim(lua_ls): fix undefined vim on nvim nightly build

--- nvim/.config/nvim/lua/lsp/lua_ls.lua
+++ nvim/.config/nvim/lua/lsp/lua_ls.lua
@@ -2,32 +2,31 @@ return {
   on_init = function(client)
     local path = client.workspace_folders[1].name
     if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-      return true
+      return
     end
 
-    client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-      Lua = {
-        runtime = {
-          version = 'LuaJIT',
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
         },
-        -- Make the server aware of Neovim runtime files
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            vim.env.VIMRUNTIME,
-          },
-          -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-          -- library = vim.api.nvim_get_runtime_file("", true)
-        },
-        format = {
-          defaultConfig = {
-            trailing_table_separator = 'always',
-            quote_style = 'single',
-          },
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      },
+      format = {
+        defaultConfig = {
+          trailing_table_separator = 'always',
+          quote_style = 'single',
         },
       },
     })
 
     client.notify('workspace/didChangeConfiguration', { settings = client.config.settings, })
   end,
+  settings = { Lua = {} }
 }
