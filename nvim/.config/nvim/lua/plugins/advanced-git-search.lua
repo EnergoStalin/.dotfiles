5743207cc09044a00b18e44794e39af122c93bd4
EnergoStalin <ens.stalin@gmail.com>
2024-05-19 13:30:26 +0300

    nvim(ags): new plugin for searching commit history

---
+++ nvim/.config/nvim/lua/plugins/advanced-git-search.lua
@@ -0,0 +1,20 @@
+return {
+  'aaronhallaert/advanced-git-search.nvim',
+  cmd = { 'AdvancedGitSearch', },
+  init = function()
+    vim.keymap.set({'x', 'o'}, 'gs', ':AdvancedGitSearch diff_commit_line<cr>', { desc = 'Advanced git search', noremap = true })
+  end,
+  config = function()
+    require('telescope').load_extension('advanced_git_search')
+  end,
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    -- to show diff splits and open commits in browser
+    'tpope/vim-fugitive',
+    -- to open commits in browser with fugitive
+    'tpope/vim-rhubarb',
+    -- optional: to replace the diff from fugitive with diffview.nvim
+    -- (fugitive is still needed to open in browser)
+    -- "sindrets/diffview.nvim",
+  },
+}
