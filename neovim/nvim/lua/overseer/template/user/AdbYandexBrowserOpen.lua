1b8821afc6b229cd7f8f8a17625bb83572ec55ef
EnergoStalin <ens.stalin@gmail.com>
2024-07-01 01:12:57 +0300

    nvim(overseer): generalize AdbYandexBrowserOpen template

--- neovim/nvim/lua/overseer/template/user/AdbYandexBrowserOpen.lua
+++ neovim/nvim/lua/overseer/template/user/AdbYandexBrowserOpen.lua
@@ -9,13 +9,14 @@ return {
     },
   },
   builder = function(o)
-    local interface = io.popen([[ip -o addr show dev eno1]]):read()
+    local gateway = string.gmatch(io.popen([[ip route | grep default]]):read(), [[dev (%w+)]])()
+    local interface = io.popen([[ip -o addr show dev ]] .. gateway):read()
     local ip = string.sub(interface, 17, string.find(interface, '/', 17) - 1)
 
     if string.find(o.url, '%%local%%') then o.url = string.gsub(o.url, '%%local%%', ip) end
 
     return {
-      name = 'adb open',
+      name = 'YandexBrowserOpen',
       cmd = {
         'adb',
         'shell',
@@ -27,16 +28,6 @@ return {
         }, ' '),
       },
       cwd = vim.fn.getcwd(),
-      components = {
-        'default',
-        'unique',
-        {
-          'dependencies',
-          task_names = {
-            'build ppixiv',
-          },
-        },
-      }
     }
   end
 }
