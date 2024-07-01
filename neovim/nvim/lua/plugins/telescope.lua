3b20f612347c12a8e50e013e84dda93ce921118a
EnergoStalin <ens.stalin@gmail.com>
2024-07-01 08:23:24 +0300

    nvim(telescope): conform ro telescope <leader>f prefix

--- neovim/nvim/lua/plugins/telescope.lua
+++ neovim/nvim/lua/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
   keys = {
     { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Fuzzy find files respecting gitignore', },
     { '<leader>fg', '<cmd>Telescope live_grep<cr>',  desc = 'Fuzzy search for file content', },
-    { '<leader>tk', '<cmd>Telescope keymaps<cr>',    desc = 'Fuzzy search keymaps', },
+    { '<leader>fk', '<cmd>Telescope keymaps<cr>',    desc = 'Fuzzy search keymaps', },
   },
   config = function()
     require('telescope').setup({
