9d9300d5cbf23c0913589e429e820398ff6214ed
EnergoStalin <ens.stalin@gmail.com>
2024-07-07 09:05:40 +0300

    nvim(tokyonight): fix transparent borders

--- neovim/nvim/lua/plugins/tokyonight-theme.lua
+++ neovim/nvim/lua/plugins/tokyonight-theme.lua
@@ -2,10 +2,20 @@ return {
   'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
-  config = function ()
+  config = function()
     require('tokyonight').setup({
       transparent = true,
+      on_highlights = function(hl)
+        local none = { bg = nil }
+        hl.FloatBorder = none
+        hl.TelescopeNormal = none
+        hl.TelescopeBorder = none
+        hl.TelescopeTitle = none
+        hl.TelescopePromptBorder = none
+        hl.TelescopePromptTitle = none
+      end,
     })
+
     vim.cmd('colorscheme tokyonight')
   end,
 }
