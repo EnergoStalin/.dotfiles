2d56d75656a3fe2317ddd375b390fd8c8d855405
EnergoStalin <ens.stalin@gmail.com>
2024-07-08 10:48:49 +0300

    nvim(LuaSnip): overseer lua snippets

---
+++ neovim/nvim/lua/snippets/lua.lua
@@ -0,0 +1,128 @@
+---@diagnostic disable: undefined-global
+
+local function param()
+  return fmt([[
+  {} = {{
+    type = '{}',
+    default = {},
+    optional = {},
+    description = '{}',
+  }},
+  ]], {
+    i(1),
+    c(2, {
+      t('string'),
+      t('number'),
+      t('list'),
+    }),
+    i(3),
+    c(4, { t('true'), t('false'), }),
+    i(0),
+  })
+end
+
+local function params()
+  return fmt([[
+  {},
+  params = {{
+    {}
+  }},
+  ]], {
+    t(''),
+    sn(1, param()),
+  })
+end
+
+local function cmd()
+  return fmt([[
+  cmd = {},
+  cwd = vim.fn.getcwd(),
+  ]], {
+    c(1, {
+      sn(nil, { t('\''), i(1), t('\''), }),
+      sn(nil, { t('{ \''), i(1), t('\', }'), }),
+    }),
+  })
+end
+
+local function deps()
+  return fmt([[
+  {{
+    'dependencies',
+    seqential = {},
+    task_names = {{
+      {},
+    }},
+  }},
+  ]], {
+    c(1, { t('true'), t('false'), }),
+    i(0),
+  })
+end
+
+local function orchestrator()
+  return fmt([[
+  ,
+  strategy = {{
+    'orchestrator',
+    tasks = {{
+      {}
+    }},
+  }},
+  ]], {
+    i(0),
+  })
+end
+
+local function run()
+  return fmt([[
+  overseer.run_template({{ name = '{}' }}{})
+  ]], {
+    i(1),
+    c(2, {
+      t(''),
+      sn(nil, { t(', { '), i(1), t(' }'), }),
+    }),
+  })
+end
+
+local function register()
+  return fmt([[
+  overseer.register_tamplate({{
+    name = '{}'{}
+    builder = function()
+      return {{
+        name = '{}'{}
+        components = {{
+          'default',{}
+        }},
+      }}
+    end
+  }})
+  ]], {
+    i(1),
+    c(2, {
+      t(','),
+      sn(nil, params()),
+    }),
+    f(function(a) return a[1] end, 1),
+    c(3, {
+      t(','),
+      sn(nil, cmd()),
+      sn(nil, orchestrator()),
+    }),
+    c(4, {
+      t(''),
+      sn(nil, deps()),
+    }),
+  })
+end
+
+
+return {
+  s('oregister', register()),
+  s('orun', run()),
+  s('oparam', param()),
+  s('odeps', deps()),
+  s('orequire', { t([[local overseer = require('overseer')]]) }),
+}
