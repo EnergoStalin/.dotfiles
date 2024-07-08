c686e49fa50d42bb7c5cd58849024cafc012029c
EnergoStalin <ens.stalin@gmail.com>
2024-07-08 06:44:04 +0300

    nvim(telescope): more picker shortcuts

--- neovim/nvim/lua/plugins/telescope.lua
+++ neovim/nvim/lua/plugins/telescope.lua
@@ -5,9 +5,14 @@ return {
     'BurntSushi/ripgrep',
   },
   keys = {
-    { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Fuzzy find files respecting gitignore', },
-    { '<leader>fg', '<cmd>Telescope live_grep<cr>',  desc = 'Fuzzy search for file content', },
-    { '<leader>fk', '<cmd>Telescope keymaps<cr>',    desc = 'Fuzzy search keymaps', },
+    { '<leader>ff', '<cmd>Telescope find_files<cr>',         desc = 'Fuzzy find files respecting gitignore', },
+    { '<leader>fg', '<cmd>Telescope live_grep<cr>',          desc = 'Fuzzy search for file content', },
+    { '<leader>fk', '<cmd>Telescope keymaps<cr>',            desc = 'Fuzzy search keymaps', },
+    { '<F4>',       '<cmd>Telescope treesitter<cr>',         desc = 'Show incoming calls', },
+    { '<F12>',      '<cmd>Telescope lsp_incoming_calls<cr>', desc = 'Show incoming calls', },
+    { '<F11>',      '<cmd>Telescope lsp_outgoing_calls<cr>', desc = 'Show outgoing calls', },
+    { '<F8>',       '<cmd>Telescope lsp_references<cr>',     desc = 'Show references', },
+    { '<leader>gs', '<cmd>Telescope git_bcommits_range<cr>', mode = { 'x', 'o', },                           desc = 'Show commits referencing selected lines', },
   },
   config = true,
   opts = {
