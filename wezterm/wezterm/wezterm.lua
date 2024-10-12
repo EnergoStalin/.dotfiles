a839c3fe5e5878cc5d9312985c3fd17ca29819f9
EnergoStalin <ens.stalin@gmail.com>
2024-10-12 15:20:08 +0300

    wezterm: tweak keybinds more

--- wezterm/wezterm/wezterm.lua
+++ wezterm/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require('wezterm')
 
+local action = wezterm.action
 local scheme = wezterm.color.get_builtin_schemes()['Edge Dark (base16)']
-scheme.background = 'rgba(20,20,20,0.6)'
+scheme.background = 'rgba(20,20,20,0.8)'
 
 return {
   enable_tab_bar = false,
@@ -10,26 +11,63 @@ return {
   color_schemes = {
     ['Transparent'] = scheme
   },
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  window_frame = {
+    border_left_width = 0,
+    border_right_width = 0,
+    border_bottom_height = 0,
+    border_top_height = 0,
+  },
 
-  font_size = 20,
+  font_size = 24,
   font = wezterm.font('JetBrains Mono'),
 
-  leader = { key = 'b', mods = 'CTRL' },
+  -- leader = { key = 'b', mods = 'CTRL' },
   keys = {
     {
+      key = 'Space',
+      mods = 'CTRL|SHIFT',
+      action = action.ActivateCopyMode,
+    },
+    {
       key = '"',
-      mods = 'LEADER|SHIFT',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+      mods = 'CTRL|SHIFT',
+      action = action.SplitVertical { domain = 'CurrentPaneDomain' },
     },
     {
       key = '%',
-      mods = 'LEADER|SHIFT',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+      mods = 'CTRL|SHIFT',
+      action = action.SplitHorizontal { domain = 'CurrentPaneDomain' },
     },
     {
-      key = 'Space',
-      mods = 'LEADER',
-      action = wezterm.action.RotatePanes 'Clockwise'
-    }
+      key = 'H',
+      mods = 'CTRL|SHIFT',
+      action = action.ActivatePaneDirection 'Left'
+    },
+    {
+      key = 'L',
+      mods = 'CTRL|SHIFT',
+      action = action.ActivatePaneDirection 'Right'
+    },
+    {
+      key = 'J',
+      mods = 'CTRL|SHIFT',
+      action = action.ActivatePaneDirection 'Down'
+    },
+    {
+      key = 'K',
+      mods = 'CTRL|SHIFT',
+      action = action.ActivatePaneDirection 'Up'
+    },
+    {
+      key = 'Z',
+      mods = 'CTRL|SHIFT',
+      action = action.TogglePaneZoomState
+    },
   },
 }
