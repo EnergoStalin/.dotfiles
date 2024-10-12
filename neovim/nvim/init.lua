74b97e62deb23a90ef4404d2846c5cfdcf1463d3
EnergoStalin <ens.stalin@gmail.com>
2024-10-12 15:22:11 +0300

    nvim(init): decompose config

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,50 +1,7 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+package.path = vim.fn.stdpath('config') .. '/?.lua;' .. package.path
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-
-vim.opt.list = true
-vim.opt.number = true
-vim.opt.expandtab = true
-vim.opt.relativenumber = true
-vim.opt.splitright = true
-vim.opt.cursorline = true
-vim.opt.listchars = 'eol:↵,lead:·,tab:⭾ '
-
-vim.opt.autoread = true
-vim.opt.autoindent = true
-
-vim.opt.langmap =
-'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
-
--- Allow reading per directory .nvim.lua files
-vim.opt.exrc = true
-
-vim.opt.foldenable = false
-vim.opt.foldlevel = 99
-vim.opt.foldmethod = 'indent'
-
-vim.keymap.set('n', 'j', 'jzz')
-vim.keymap.set('n', 'k', 'kzz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-
-vim.keymap.set('n', ',d', '<cmd>b#<bar>bd#<CR>')
-
-vim.keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<cr>', { desc = 'open diagnostic quickfix list', })
-vim.keymap.set('n', '<leader>k', ':vertical botright help <C-r><C-w><cr>',
-  { desc = 'Open help on subject under cursor', })
-
-vim.keymap.set('n', '<C-e>', '<cmd>qa<CR>', { desc = 'quit neovim', })
-
--- Always resource .nvim.lua after writing
-vim.cmd([[
-  augroup nvim_source
-    autocmd!
-    autocmd BufWritePost .nvim.lua source <afile>
-  augroup END
-]])
+require('options')
+require('netrw')
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 ---@diagnostic disable-next-line: undefined-field
---
+++ neovim/nvim/options.lua
@@ -0,0 +1,36 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.relativenumber = true
+vim.opt.splitright = true
+vim.opt.cursorline = true
+
+vim.opt.list = true
+vim.opt.listchars = 'eol:↵,lead:·,tab:⭾ '
+
+vim.opt.autoread = true
+vim.opt.autoindent = true
+
+vim.opt.langmap =
+'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
+
+vim.opt.exrc = true
+
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = 'indent'
+
+-- Always resource .nvim.lua after writing
+vim.cmd([[
+  augroup nvim_source
+    autocmd!
+    autocmd BufWritePost .nvim.lua source <afile>
+  augroup END
+]])
+
+vim.keymap.set('n', '<leader>,d', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>')
