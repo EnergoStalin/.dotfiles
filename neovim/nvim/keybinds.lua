56e38e45e97257b668577a266e171e977b061457
EnergoStalin <ens.stalin@gmail.com>
2024-10-16 16:01:56 +0300

    nvim(keybinds): remap bd

--- neovim/nvim/keybinds.lua
+++ neovim/nvim/keybinds.lua
@@ -1,3 +1,3 @@
-vim.keymap.set('n', '<leader>,d', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>')
+vim.keymap.set('n', '<C-d>', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>')
 vim.keymap.set('n', '<C-n>', '<cmd>bn<CR>')
 vim.keymap.set('n', '<C-p>', '<cmd>bp<CR>')
