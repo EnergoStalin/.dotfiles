dedb4d8e2fa5c2581750b2957c01d63ddbf02551
EnergoStalin <ens.stalin@gmail.com>
2024-10-28 05:50:17 +0300

    nvim(keybinds): get rid of pkexec to save file

--- neovim/nvim/keybinds.lua
+++ neovim/nvim/keybinds.lua
@@ -1,5 +1,3 @@
 vim.keymap.set('n', '<leader>,d', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>')
 vim.keymap.set('n', '<C-n>', '<cmd>bn<CR>')
 vim.keymap.set('n', '<C-p>', '<cmd>bp<CR>')
-
-vim.cmd[[cnoreabbrev w!! :execute ':silent write !pkexec --keep-cwd tee % > /dev/null' | :edit!]]
