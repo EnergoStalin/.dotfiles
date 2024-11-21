66d1c698bf82c817e85d983647d4820887612202
EnergoStalin <ens.stalin@gmail.com>
2024-11-21 15:19:41 +0300

    nvim(lua_ls): modernize and add vim.env.RUNTIME back

--- neovim/nvim/lua/lsp/lua_ls.lua
+++ neovim/nvim/lua/lsp/lua_ls.lua
@@ -1,24 +1,22 @@
 return {
   on_init = function(client)
-    local path = client.workspace_folders and client.workspace_folders[1].name or vim.fn.getcwd()
-
-    ---@diagnostic disable-next-line: undefined-field
-    if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-      return
+    if client.workspace_folders then
+      --- @type string
+      local path = client.workspace_folders[1].name
+      if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+        return
+      end
     end
 
     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
       runtime = {
         version = 'LuaJIT',
       },
-      -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
         library = {
-          vim.env.VIMRUNTIME,
-        },
-        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-        -- library = vim.api.nvim_get_runtime_file("", true)
+          vim.env.VIMRUNTIME
+        }
       },
       format = {
         defaultConfig = {
