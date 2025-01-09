5080beb895e4c446e2f848d14092fdd3caab4f9b
EnergoStalin <ens.stalin@gmail.com>
2025-01-09 04:53:05 +0300

    feat(yazi): change from hcextractor to gameripper
    https://gitlab.com/gameripper/gameripper

--- yazi/yazi/plugins/hcextract.yazi/init.lua
+++ yazi/yazi/plugins/hcextract.yazi/init.lua
@@ -6,24 +6,27 @@ local function info(content)
   }
 end
 
-local hovered_url = ya.sync(function()
-  local h = cx.active.current.hovered
-  return h and h.url
+local hovered = ya.sync(function()
+  return tostring(cx.active.current.hovered and cx.active.current.hovered.url)
 end)
+
+local cwd = ya.sync(function ()
+  return tostring(cx.active.current.cwd)
+end)
+
+local function sh(cmd)
+  return Command('sh'):arg('-c'):arg(cmd):status()
+end
 
 return {
   entry = function()
-    local hovered = tostring(hovered_url())
-    if not hovered then
-      return info('No file hovered')
-    end
+    local tmp = '/tmp/gameripper/'
+    Command('mkdir'):arg('-p'):arg(tmp):status()
 
-    local output, err = Command('./hcextract')
-        :arg(hovered)
-        :output()
-    if not output then
-      return info('Failed to run hcextract, error: ' .. err)
-    end
+    Command('cp'):arg(hovered()):arg(tmp):status()
+    Command('./gameripper'):arg('-d'):arg(tmp):status()
+    sh(string.format([[find '%s' -type f -name '*@*' -print0 | xargs -0 mv -t '%s']], tmp, cwd()))
+    sh(string.format([[rm -rf '%s*']], tmp))
 
     info('Characters extracted!')
   end,
