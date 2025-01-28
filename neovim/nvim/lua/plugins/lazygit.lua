99e88348a28a482ec6fe092ab8a6bd18636f48a5
EnergoStalin <ens.stalin@gmail.com>
2025-01-28 06:41:32 +0300

    nvim(lazygit): use full window

--- neovim/nvim/lua/plugins/lazygit.lua
+++ neovim/nvim/lua/plugins/lazygit.lua
@@ -3,7 +3,14 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
+  config = function ()
+    vim.g.lazygit_floating_window_scaling_factor = 1
+  end,
   keys = {
-    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit', },
+    {
+      '<leader>lg',
+      '<cmd>LazyGit<cr>',
+      desc = 'Open LazyGit floating window',
+    },
   },
 }
