dfe6e96a69285ba8b27c9312c73a9e142b7b94d4
EnergoStalin <ens.stalin@gmail.com>
2025-02-12 23:40:38 +0300

    nvim(s21): use get current day in init-folders instead of arg

--- neovim/nvim/s21/sql/init-folders.lua
+++ neovim/nvim/s21/sql/init-folders.lua
@@ -1,5 +1,6 @@
 local overseer = require('overseer')
 local config = require('s21.sql.config')
+local ex = require('s21.sql.ex')
 
 overseer.register_template({
   name = 'init folders',
@@ -8,16 +9,12 @@ overseer.register_template({
       type = 'number',
       name = 'Task count',
     },
-    day_number = {
-      type = 'number',
-      name = 'Day number',
-    },
   },
   builder = function(p)
     return {
       name = 'init folders',
       cmd = { 'sh', vim.fs.joinpath(config.data_path, 'init.sh'), },
-      env = { N = p.count, DN = p.day_number, },
+      env = { N = p.count, DN = ex.get_current_day(), },
       components = { 'default', 'unique', },
     }
   end,
