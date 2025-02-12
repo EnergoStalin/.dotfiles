640fb18216e89df0d173680a6c9a2b675d1261da
EnergoStalin <ens.stalin@gmail.com>
2025-02-12 22:53:34 +0300

    nvim(keybinds): less keys are better also i'm tired to do :nohl everytime so space h will do the job

--- neovim/nvim/keybinds.lua
+++ neovim/nvim/keybinds.lua
@@ -1,3 +1,4 @@
-vim.keymap.set('n', '<leader>,d', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>zz')
+vim.keymap.set('n', '<leader>d', '<cmd>bp<bar>sp<bar>bn<bar>bd<CR>zz')
+vim.keymap.set('n', '<leader>h', '<cmd>nohl<CR>')
 vim.keymap.set('n', '<C-n>', '<cmd>bn<CR>')
 vim.keymap.set('n', '<C-p>', '<cmd>bp<CR>')
