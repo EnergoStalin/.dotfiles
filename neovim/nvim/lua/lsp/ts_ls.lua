75ca5bfcbb7dbab6b103ce0bc3c0d795a9091723
EnergoStalin <ens.stalin@gmail.com>
2025-03-14 18:29:14 +0300

    nvim(lsp): ts_ls use tsgo if avaliable

--- neovim/nvim/lua/lsp/ts_ls.lua
+++ neovim/nvim/lua/lsp/ts_ls.lua
@@ -2,5 +2,7 @@ local util = require 'lspconfig.util'
 
 return {
   single_file_support = false,
-  root_dir = util.root_pattern('tsconfig.json', 'jsconfig.json', 'package.json')
+  root_dir = util.root_pattern('tsconfig.json', 'jsconfig.json', 'package.json'),
+
+  vim.fn.executable('tsgo') and unpack({ cmd = { 'tsgo', 'lsp', '--stdio', } }) or nil,
 }
