36636729c95e0d1e0b87813f10472029eeac6450
EnergoStalin <ens.stalin@gmail.com>
2025-03-19 04:40:34 +0300

    nvim(options): align

--- neovim/nvim/options.lua
+++ neovim/nvim/options.lua
@@ -1,25 +1,25 @@
-vim.g.mapleader = ' '
+vim.g.mapleader      = ' '
 vim.g.maplocalleader = ' '
 
-vim.opt.backup = false
+vim.opt.backup      = false
 vim.opt.writebackup = false
-vim.opt.updatetime = 300
-vim.opt.signcolumn = "yes"
+vim.opt.updatetime  = 300
+vim.opt.signcolumn  = "yes"
 
-vim.opt.tabstop = 2
+vim.opt.tabstop    = 2
 vim.opt.shiftwidth = 2
 
-vim.opt.number = true
-vim.opt.expandtab = true
+vim.opt.number         = true
+vim.opt.expandtab      = true
 vim.opt.relativenumber = true
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.cursorline = true
+vim.opt.splitright     = true
+vim.opt.splitbelow     = true
+vim.opt.cursorline     = true
 
-vim.opt.list = true
+vim.opt.list      = true
 vim.opt.listchars = 'eol:,lead:·,tab:⭾ '
 
-vim.opt.autoread = true
+vim.opt.autoread   = true
 vim.opt.autoindent = true
 
 vim.opt.langmap =
@@ -28,7 +28,7 @@ vim.opt.langmap =
 vim.opt.exrc = true
 
 vim.opt.foldenable = false
-vim.opt.foldlevel = 99
+vim.opt.foldlevel  = 99
 vim.opt.foldmethod = 'indent'
 
 vim.api.nvim_create_autocmd("BufWritePost", {
@@ -42,7 +42,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
   callback = function()
     if vim.bo.buftype ~= "terminal" then return end
 
-    vim.wo.number = false
+    vim.wo.number         = false
     vim.wo.relativenumber = false
   end,
   group = vim.api.nvim_create_augroup("term_open", { clear = true })
