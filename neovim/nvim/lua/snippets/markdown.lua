aa794db0552c4adf863b6c39f5b7da0c771b9b72
EnergoStalin <ens.stalin@gmail.com>
2025-03-20 21:18:46 +0300

    nvim(luasnip/markdown): add details snippet

---
+++ neovim/nvim/lua/snippets/markdown.lua
@@ -0,0 +1,17 @@
+---@diagnostic disable: undefined-global
+
+local function details()
+	return fmt([[
+		<details>
+			<summary>{}</summary>
+
+			{}
+		</details>{}
+	]], {
+		i(1, 'summary'), i(2), i(0)
+	})
+end
+
+return {
+	s('details', details())
+}
