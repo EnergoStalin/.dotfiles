887435ce2918aa539caef4db4485aa5521ef2032
EnergoStalin <ens.stalin@gmail.com>
2025-04-22 21:55:38 +0300

    qbittorent(fix): theme patch not applying on enpty config

--- qbittorrent/prepare.sh
+++ qbittorrent/prepare.sh
@@ -7,8 +7,14 @@ PWD_ESCAPED="$(pwd | sed 's/\//\\\//g')"
 curl -sLO https://github.com/dracula/qbittorrent/raw/master/dracula.qbtheme
 
 patch_theme() {
-  sed -E 's/(General\\CustomUIThemePath=).*$/\1'$PWD_ESCAPED'\/dracula.qbtheme/' \
-    -i "$CONFIG/qBittorrent/qBittorrent.conf"
+  p="$CONFIG/qBittorrent/qBittorrent.conf"
+  grep "$p" -e CustomUIThemePath && \
+  sed -E 's/(General\\CustomUIThemePath=).*$/\1'$PWD_ESCAPED'\/dracula.qbtheme/' -i "$p" || \
+  cat <<-EOF >> "$p"
+[Preferences]
+General\\UseCustomUITheme=true
+General\\CustomUIThemePath=$PWD/dracula.qbtheme
+EOF
 }
 
 patch_theme
