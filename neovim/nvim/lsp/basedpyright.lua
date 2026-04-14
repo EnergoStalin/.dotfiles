29df4fdba772512778ab305c25bc6abac78aa27f
EnergoStalin <ens.stalin@gmail.com>
2026-04-14 23:19:31 +0300

    nvim(lsp/basedpyright): disable in django projects

--- neovim/nvim/lsp/basedpyright.lua
+++ neovim/nvim/lsp/basedpyright.lua
@@ -1,7 +1,19 @@
-vim.lsp.inlay_hint.enable()
-
+---@type vim.lsp.Config
 return {
   root_markers = { '.venv' },
+  root_dir = function(_, on_dir)
+    local cwd = vim.fn.getcwd()
+    if vim.system({
+          'grep',
+          '--include', '*.py',
+          '--exclude-dir', '.venv',
+          '--exclude-dir', '.git',
+          '-Rm', '1', '.',
+          '-e', 'django'
+        }):wait().code ~= 0 then
+      on_dir(cwd)
+    end
+  end,
   settings = {
     basedpyright = {
       analysis = {
