5d3e5f17b034e1d78a0351e123f8fb9f56784839
EnergoStalin <ens.stalin@gmail.com>
2026-04-16 22:52:45 +0300

    nvim(lsp/basedpyright): do not disable on django projects
    better off setting typeChecking to basic in pyproject.toml

--- neovim/nvim/lsp/basedpyright.lua
+++ neovim/nvim/lsp/basedpyright.lua
@@ -1,19 +1,6 @@
 ---@type vim.lsp.Config
 return {
   root_markers = { '.venv' },
-  root_dir = function(_, on_dir)
-    local cwd = vim.fn.getcwd()
-    if vim.system({
-          'grep',
-          '--include', '*.py',
-          '--exclude-dir', '.venv',
-          '--exclude-dir', '.git',
-          '-Rm', '1', '.',
-          '-e', 'django'
-        }):wait().code ~= 0 then
-      on_dir(cwd)
-    end
-  end,
   settings = {
     basedpyright = {
       analysis = {
