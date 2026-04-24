6347711f630a34873d1f25bcfa3a0a4838843f9e
EnergoStalin <ens.stalin@gmail.com>
2026-04-25 02:12:23 +0300

    nvim(tabular): by #

--- neovim/nvim/lua/plugins/tabular.lua
+++ neovim/nvim/lua/plugins/tabular.lua
@@ -6,5 +6,6 @@ return {
     { '<leader>a:', ":'<,'>Tabularize /^[^:]*\\zs/l1r0<cr>", mode = { 'v', }, noremap = true, },
     { '<leader>a=', ":'<,'>Tabularize /^[^=]*\\zs/l1r0<cr>", mode = { 'v', }, noremap = true, },
     { '<leader>a|', ":'<,'>Tabularize /|<cr>",               mode = { 'v', }, noremap = true, },
+    { '<leader>a#', ":'<,'>Tabularize /#<cr>",               mode = { 'v', }, noremap = true, },
   },
 }
