a1c50bd889fe1260697d7048d4bd15a126233428
EnergoStalin <ens.stalin@gmail.com>
2026-04-25 02:12:54 +0300

    nvim(conform/python): full ruff chain

--- neovim/nvim/lua/plugins/conform.lua
+++ neovim/nvim/lua/plugins/conform.lua
@@ -9,7 +9,7 @@ return {
     },
     formatters_by_ft = {
       ['_'] = { 'trim_whitespace' },
-      python = { 'ruff_format' },
+      python = { 'ruff_format', 'ruff_fix', 'ruff_organize_imports' },
       rust = { 'rustfmt', lsp_format = 'fallback' },
       lua = { lsp_format = 'fallback' },
     }
